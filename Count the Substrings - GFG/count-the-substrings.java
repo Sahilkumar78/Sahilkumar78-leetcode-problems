ce3ed5c97abcef28b9cf34181c580fd947a576ae
Sahilkumar78 <120403023+Sahilkumar78@users.noreply.github.com>
2023-06-13 10:43:30 +0530

    Added solution - LeetHub

---
+++ Count the Substrings - GFG/count-the-substrings.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0){
+            String S = read.readLine().trim();
+            Solution ob = new Solution();
+            int ans = ob.countSubstring(S);
+            System.out.println(ans);
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    int countSubstring(String S) 
+    { 
+        // code here
+     int count = 0;
+      
+      for(int i = 0; i <S.length(); i++){
+          int upperCase = 0;
+          int lowerCase = 0;
+          
+          for(int j = i ; j <S.length(); j++){
+               
+              char c= S.charAt(j);
+              
+              if(Character.isUpperCase(c)){
+                  upperCase++;
+              }else{
+                  lowerCase++;
+              }
+              if(lowerCase==upperCase){
+                   count++;
+              }
+          }
+          
+          
+      }
+      return count;
+    }
+} 
