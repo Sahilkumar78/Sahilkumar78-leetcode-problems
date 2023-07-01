10cffa5142774020e6eb3e53f54da244048c6002
Sahilkumar78 <120403023+Sahilkumar78@users.noreply.github.com>
2023-07-01 20:35:42 +0530

    Time: 0 ms (100.00%), Space: 43.9 MB (46.95%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+      private long minVal = Long.MIN_VALUE;
+   //  public long minVal = Integer.Max_Value;
+    public boolean isValidBST(TreeNode root) {
+        
+         
+            if (root == null) return true; 
+        if (!isValidBST(root.left)) return false;
+        
+        if (minVal >= root.val) return false; 
+
+        minVal = root.val;
+
+        if (!isValidBST(root.right)) return false;
+
+        return true;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
